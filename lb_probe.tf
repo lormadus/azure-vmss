ae26b11b0103eca8e8fd8fe67017afae57d48771
David Yoon <david.hyeshik@yahoo.com>
2020-09-11 09:56:38 +0900

    Create lb_probe.tf

---
+++ lb_probe.tf
@@ -0,0 +1,8 @@
+resource "azurerm_lb_probe" “user01-lb-probe” {
+    resource_group_name = azurerm_resource_group.user01-rg.name
+    loadbalancer_id = azurerm_lb.user01-lb.id
+    name = "http-probe"
+    protocol = "Http"
+    request_path = "/"
+    port = 80
+}
