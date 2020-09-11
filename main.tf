9d62556fc9486c8334a19f40660bcf8458051814
David Yoon <david.hyeshik@yahoo.com>
2020-09-11 09:36:57 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+    subscription_id = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    client_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    client_secret   = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    tenant_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx”
+    features {}
+}
