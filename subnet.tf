69090de972e8e1fffc4f05b889bbdf52a9e7fc91
David Yoon <david.hyeshik@yahoo.com>
2020-09-11 09:45:44 +0900

    Create subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,6 @@
+resource "azurerm_subnet" “user01-subnet1" {
+    name = ”user01-mysubnet1"
+    resource_group_name = azurerm_resource_group.user01-rg.name
+    virtual_network_name = azurerm_virtual_network.user01-vnet.name
+    address_prefix = "10.0.1.0/24"
+}
