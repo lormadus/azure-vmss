ff1bf6894de1cbd9b9278122010cb0380a2734e8
David Yoon <david.hyeshik@yahoo.com>
2020-09-21 13:36:37 +0900

    Update lb_rule.tf

--- lb_rule.tf
+++ lb_rule.tf
@@ -1,11 +1,11 @@
 resource "azurerm_lb_rule" "lbnatrule" {
     resource_group_name = azurerm_resource_group.user01-rg.name
-    loadbalancer_id = azurerm_lb.example.id
+    loadbalancer_id = azurerm_lb.user01-lb.id
     name = "http"
     protocol = "Tcp"
     frontend_port = 80
     backend_port = 80
     backend_address_pool_id = azurerm_lb_backend_address_pool.bpepool.id
-    frontend_ip_configuration_name = "PublicIPAddress"
-    probe_id = azurerm_lb_probe.example.id
+    frontend_ip_configuration_name = "user01PublicIPAddress"
+    probe_id = azurerm_lb_probe.user01-lb-probe.id
 }
