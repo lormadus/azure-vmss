afb260a5367649e5e6d5e33c43c4a9f952af0b3a
David Yoon <david.hyeshik@yahoo.com>
2020-09-21 13:33:32 +0900

    Update lb_probe.tf

--- lb_probe.tf
+++ lb_probe.tf
@@ -1,4 +1,4 @@
-resource "azurerm_lb_probe" “user01-lb-probe” {
+resource "azurerm_lb_probe" "user01-lb-probe" {
     resource_group_name = azurerm_resource_group.user01-rg.name
     loadbalancer_id = azurerm_lb.user01-lb.id
     name = "http-probe"
