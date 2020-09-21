185df0c85747fdd408cb1a11e4ca5a5823eaebcf
David Yoon <david.hyeshik@yahoo.com>
2020-09-21 13:31:37 +0900

    Update vmss.tf

--- vmss.tf
+++ vmss.tf
@@ -12,7 +12,7 @@ sku {
 storage_profile_image_reference {
     publisher = "Canonical"
     offer = "UbuntuServer"
-    sku = “18.04-LTS"
+    sku = "18.04-LTS"
     version = "latest"
 }
 
@@ -30,8 +30,8 @@ storage_profile_data_disk {
 }
 os_profile {
     computer_name_prefix = "testvm"
-    admin_username = "myadmin”  ## VM 에 접속할 계정
-    custom_data = file(“web.sh”)
+    admin_username = "myadmin"  ## VM 에 접속할 계정
+    custom_data = file("web.sh")
 }
 
 #서버 80포트 접속안되시는 분들은 실제 서버 접속하셔서 아파치 데몬이 정상 동작하는지
@@ -48,7 +48,7 @@ os_profile {
 os_profile_linux_config {
 disable_password_authentication = true
 ssh_keys {
-    path = "/home/myadmin/.ssh/authorized_keys“   ## pwd 실행후 경로설정 ex) /home/user01 등 
+    path = "/home/myadmin/.ssh/authorized_keys"   ## pwd 실행후 경로설정 ex) /home/user01 등 
     key_data = file("~/.ssh/id_rsa.pub")  ## 터미널에서 ssh-keygen 으로 생성 (엔터 3번) 
     }
 }
