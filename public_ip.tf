dc95115fb756453630835f9e770f355baedc1a0d
David Yoon <david.hyeshik@yahoo.com>
2020-09-21 13:32:53 +0900

    Update public_ip.tf

--- public_ip.tf
+++ public_ip.tf
@@ -1,12 +1,12 @@
-resource "azurerm_public_ip" “user01-publicip" {  
-name                = “mypublicIP"  
+resource "azurerm_public_ip" "user01-publicip" {  
+name                = "mypublicIP"  
 location            = azurerm_resource_group.user01-rg.location  
 resource_group_name = azurerm_resource_group.user01-rg.name  
 allocation_method   = "Static"  
 domain_name_label   = azurerm_resource_group.user01-rg.name  
 
 ## 동일 Region에 추가 Public IP 생성시에는 아래처러 FQDN(Full Query Domain Name)으로 직접 입력
-# domain_name_label   = “user01pubip2.westus.cloudapp.azure.com”
+# domain_name_label   = "user01pubip2.westus.cloudapp.azure.com"
 
 
 	tags = {    
