3e9d620a91f8618db03d64af9ae6f8786c572933
David Yoon <david.hyeshik@yahoo.com>
2020-10-28 15:00:14 +0900

    Update public_ip.tf

--- public_ip.tf
+++ public_ip.tf
@@ -4,8 +4,9 @@ location            = azurerm_resource_group.user01-rg.location
 resource_group_name = azurerm_resource_group.user01-rg.name  
 allocation_method   = "Static"  
 domain_name_label   = azurerm_resource_group.user01-rg.name  
-
-## 동일 Region에 추가 Public IP 생성시에는 아래처럼 직접 입력
+	
+## Resource Group 이름에 대문자를 쓰면 에러 발생 (so, 리소스 그룹은 소문자로 생성)
+## 동일 Region에 추가 Public IP 생성시에는 기존 Public IP와 다른 이름을 할당해야 함으로 아래처럼 직접 입력해도 됨
 # domain_name_label   = "user01pubip2"
 
 
