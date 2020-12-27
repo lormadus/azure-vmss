521125eb6537c9d75400be0d28772f975a74c625
David Yoon <david.hyeshik@yahoo.com>
2020-12-27 22:32:51 +0900

    Create myNetworkSecurityGroup.tf

---
+++ myNetworkSecurityGroup.tf
@@ -0,0 +1,32 @@
+resource "azurerm_network_security_group" "user01nsg" {
+    name                = "user01nsg"
+    location            = azurerm_resource_group.user01-rg.location
+    resource_group_name = azurerm_resource_group.user01-rg.name
+    
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+ security_rule {
+        name                       = "HTTP"
+        priority                   = 2001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
