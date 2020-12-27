c1243c03f3a9f9c43b67d856ad92c06b75292947
David Yoon <david.hyeshik@yahoo.com>
2020-12-27 22:28:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
 provider "azurerm" {
-    subscription_id = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-    client_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-    client_secret   = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-    tenant_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx”
     features {}
 }
