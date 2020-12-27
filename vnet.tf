042675dff0197be2fd4b523e748cdba390f0cd5f
David Yoon <david.hyeshik@yahoo.com>
2020-12-27 22:30:58 +0900

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
-resource "azurerm_virtual_network" "user01-vnet" {
-	name 			= "user01-myVnet"
-	address_space 		= ["1.0.0.0/16"]
-	location 			= azurerm_resource_group.user01-rg.location
+resource "azurerm_subnet" "user01-subnet1" {
+	name 		      	= "user01-mysubnet1"
 	resource_group_name	= azurerm_resource_group.user01-rg.name
+	virtual_network_name	= azurerm_virtual_network.user01-vnet.name
+	address_prefixes 	= ["1.0.1.0/24"]
 }
