5ef71a8bb802f80fb18dac6a5df6d4ff491c0a42
David Yoon <david.hyeshik@yahoo.com>
2020-12-27 22:36:46 +0900

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -1,11 +1,11 @@
 resource "azurerm_lb" "user01-lb" {
-  name 			              = "user01lb"
-  location 		            = azurerm_resource_group.user01-rg.location
-  resource_group_name 	  = azurerm_resource_group.user01-rg.name
+  name                    = "user01lb"
+  location                = azurerm_resource_group.user01-rg.location
+  resource_group_name     = azurerm_resource_group.user01-rg.name
   
   frontend_ip_configuration {
-    name 			            = "user01PublicIPAddress"
-    public_ip_address_id 	= azurerm_public_ip.user01-publicip.id
+    name                  = "user01PublicIPAddress"
+    public_ip_address_id   = azurerm_public_ip.user01-publicip.id
   }
 }
 
