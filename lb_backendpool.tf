b25be72d6600e08851243054e000b3c6b9360b28
David Yoon <david.hyeshik@yahoo.com>
2020-12-27 22:37:41 +0900

    Update lb_backendpool.tf

--- lb_backendpool.tf
+++ lb_backendpool.tf
@@ -1,5 +1,6 @@
 resource "azurerm_lb_backend_address_pool" "user01-bpepool" {
-    resource_group_name = azurerm_resource_group.user01-rg.name
-    loadbalancer_id = azurerm_lb.user01-lb.id
     name = "user01-BackEndAddressPool"
+    resource_group_name = azurerm_resource_group.user01-rg.name
+    loadbalancer_id     = azurerm_lb.user01-lb.id
 }
+
