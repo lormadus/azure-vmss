df15105d6cc5909258abb7f74614dc98625895ea
David Yoon <david.hyeshik@yahoo.com>
2021-07-05 16:00:06 +0900

    Update web.sh

--- web.sh
+++ web.sh
@@ -4,10 +4,12 @@
 #Ubuntu 사용하시는 경우
 apt-get update -y
 apt-get install -y apache2
+service apache2 start
 
 #CentOS, RHEL 사용하시는 경우
 #yum update –y
 #yum install –y apache2
+#/bin/systemctl start httpd
 
 echo "<html>" > /var/www/html/index.html   ## 리다이렉션 반드시 한개로
 echo "Hello World from $(hostname -f)" >> /var/www/html/index.html
