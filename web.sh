67d04dcf713083c8ac3ef5014ed54acffe31f28e
David Yoon <david.hyeshik@yahoo.com>
2021-07-05 14:57:17 +0900

    Update web.sh

--- web.sh
+++ web.sh
@@ -11,5 +11,5 @@ apt-get install -y apache2
 
 echo "<html>" > /var/www/html/index.html   ## 리다이렉션 반드시 한개로
 echo "Hello World from $(hostname -f)" >> /var/www/html/index.html
-echo "<img src=\"https://CDN_URL\">" >> "/var/www/html/index.html
+echo "<img src=\"https://CDN_URL\">" >> /var/www/html/index.html
 echo "</html>" >> /var/www/html/index.html
