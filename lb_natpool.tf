757458a1cec85eeb200f122dc13ff9e650868fa5
David Yoon <david.hyeshik@yahoo.com>
2021-07-19 16:18:04 +0900

    Update lb_natpool.tf

--- lb_natpool.tf
+++ lb_natpool.tf
@@ -1,4 +1,4 @@
-resource "azurerm_lb_nat_pool" "lbnatpool" {
+resource "azurerm_lb_nat_pool" "user01-lbnatpool" {
     resource_group_name = azurerm_resource_group.user01-rg.name
     name = "ssh"
     loadbalancer_id = azurerm_lb.user01-lb.id
