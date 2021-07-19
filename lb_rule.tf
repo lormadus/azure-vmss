43112722ca9733722df4722bcd8c32061e9ebda2
David Yoon <david.hyeshik@yahoo.com>
2021-07-19 16:17:42 +0900

    Update lb_rule.tf

--- lb_rule.tf
+++ lb_rule.tf
@@ -1,4 +1,4 @@
-resource "azurerm_lb_rule" "lbnatrule" {
+resource "azurerm_lb_rule" "user01-lbnatrule" {
     resource_group_name = azurerm_resource_group.user01-rg.name
     loadbalancer_id = azurerm_lb.user01-lb.id
     name = "http"
